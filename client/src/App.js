8d514a5c131b12a4c017ad6efbc93216eb90fa0e
Simeon Steward <simeontsteward@gmail.com>
2023-10-18 20:41:48 -0500

    Banner Background Works

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
-import Banner from './Banner';
+import logo from "./logo.svg";
+import "./App.css";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="max-h-screen bg-gray-200">
-      <Banner message="Hello" />
+      <Banner message="Hello" title="Abby's Road"
+         />
       <div className="p-4">
-        {/* Your content here */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+          suscipit tellus. Donec id leo id quam fringilla posuere. Mauris et
+          lectus a ex fermentum viverra. Nunc vel urna ut turpis dignissim
+          luctus a et metus. Fusce non semper velit. Curabitur nec nisi et
+          tortor dictum tincidunt eu a ligula. Sed nec urna in elit pharetra
+          facilisis nec vel neque. Vestibulum sed ante vitae turpis hendrerit
+          egestas. Suspendisse congue tortor vel ante malesuada, vel luctus ex
+          consectetur. Integer ac libero ut nisl dignissim euismod. Nullam et
+          sem at arcu lacinia gravida. Integer eu sapien in ante feugiat congue
+          a ut turpis. Nullam hendrerit odio quam, sit amet fermentum purus
+          convallis eu. In non finibus nunc, nec gravida urna. Praesent
+          bibendum, neque nec interdum tempor, libero purus viverra ligula, vel
+          cursus orci libero a leo. Integer bibendum tristique purus, in
+          efficitur tellus congue in.
+        </p>
+        <p>
+          Fusce efficitur, ante vel volutpat imperdiet, arcu purus pellentesque
+          ex, eu vestibulum quam ipsum vel lorem. Sed gravida auctor fringilla.
+          Vivamus a dui quis libero fermentum efficitur. Vestibulum vel urna in
+          ex varius sollicitudin. Nullam a turpis vel massa vehicula volutpat eu
+          vel nunc. Phasellus interdum sed turpis at ultrices. Vivamus sed erat
+          a odio luctus rhoncus a non turpis. Vivamus auctor, ligula et
+          tincidunt convallis, justo libero malesuada ex, eu vestibulum felis
+          quam a sapien.
+        </p>{" "}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+          suscipit tellus. Donec id leo id quam fringilla posuere. Mauris et
+          lectus a ex fermentum viverra. Nunc vel urna ut turpis dignissim
+          luctus a et metus. Fusce non semper velit. Curabitur nec nisi et
+          tortor dictum tincidunt eu a ligula. Sed nec urna in elit pharetra
+          facilisis nec vel neque. Vestibulum sed ante vitae turpis hendrerit
+          egestas. Suspendisse congue tortor vel ante malesuada, vel luctus ex
+          consectetur. Integer ac libero ut nisl dignissim euismod. Nullam et
+          sem at arcu lacinia gravida. Integer eu sapien in ante feugiat congue
+          a ut turpis. Nullam hendrerit odio quam, sit amet fermentum purus
+          convallis eu. In non finibus nunc, nec gravida urna. Praesent
+          bibendum, neque nec interdum tempor, libero purus viverra ligula, vel
+          cursus orci libero a leo. Integer bibendum tristique purus, in
+          efficitur tellus congue in.
+        </p>
+        <p>
+          Fusce efficitur, ante vel volutpat imperdiet, arcu purus pellentesque
+          ex, eu vestibulum quam ipsum vel lorem. Sed gravida auctor fringilla.
+          Vivamus a dui quis libero fermentum efficitur. Vestibulum vel urna in
+          ex varius sollicitudin. Nullam a turpis vel massa vehicula volutpat eu
+          vel nunc. Phasellus interdum sed turpis at ultrices. Vivamus sed erat
+          a odio luctus rhoncus a non turpis. Vivamus auctor, ligula et
+          tincidunt convallis, justo libero malesuada ex, eu vestibulum felis
+          quam a sapien.
+        </p>{" "}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+          suscipit tellus. Donec id leo id quam fringilla posuere. Mauris et
+          lectus a ex fermentum viverra. Nunc vel urna ut turpis dignissim
+          luctus a et metus. Fusce non semper velit. Curabitur nec nisi et
+          tortor dictum tincidunt eu a ligula. Sed nec urna in elit pharetra
+          facilisis nec vel neque. Vestibulum sed ante vitae turpis hendrerit
+          egestas. Suspendisse congue tortor vel ante malesuada, vel luctus ex
+          consectetur. Integer ac libero ut nisl dignissim euismod. Nullam et
+          sem at arcu lacinia gravida. Integer eu sapien in ante feugiat congue
+          a ut turpis. Nullam hendrerit odio quam, sit amet fermentum purus
+          convallis eu. In non finibus nunc, nec gravida urna. Praesent
+          bibendum, neque nec interdum tempor, libero purus viverra ligula, vel
+          cursus orci libero a leo. Integer bibendum tristique purus, in
+          efficitur tellus congue in.
+        </p>
+        <p>
+          Fusce efficitur, ante vel volutpat imperdiet, arcu purus pellentesque
+          ex, eu vestibulum quam ipsum vel lorem. Sed gravida auctor fringilla.
+          Vivamus a dui quis libero fermentum efficitur. Vestibulum vel urna in
+          ex varius sollicitudin. Nullam a turpis vel massa vehicula volutpat eu
+          vel nunc. Phasellus interdum sed turpis at ultrices. Vivamus sed erat
+          a odio luctus rhoncus a non turpis. Vivamus auctor, ligula et
+          tincidunt convallis, justo libero malesuada ex, eu vestibulum felis
+          quam a sapien.
+        </p>
       </div>
     </div>
   );
--- client/src/Banner.js
+++ client/src/Banner.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Banner(props) {
   return (
-    <div className='bg-blue-500 text-white p-4 w-full'>
-      {props.message}
-      <p>This is a banner message.</p>
+    <div
+      className="bg-blue-500 text-white p-4 w-full h-screen "
+      style={{
+        backgroundImage: `url('/road.jpg')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="flex flex-col justify-center items-center backdrop-blur-sm absolute inset-0">
+        <h1 className="text-6xl font-bold text-center mt-4">{props.title}</h1>
+        {props.message}
+      </div>
     </div>
   );
 }
-
 
 export default Banner;
